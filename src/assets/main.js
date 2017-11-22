1f08a985b04f6a82cf1931133d55eb50c28aaa34
bhanu18v <bhanu18v@gmail.com>
2017-11-22 12:05:32 +0100

    Done

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,15 +2,107 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
+    
+    let inputElement = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer.value==''||attempt.value ==''){
+    setHiddenFields();
+    }
+    if(!validateInput(inputElement.value)){ 
+        return false;
+    }
+    else{
+    attempt.value++;
+    }
+    if(getResults(inputElement.value)){
+    setMessage('You Win! :)');
+    showAnswer(true);
+    showReplay();
+    }
+    else {
+       if(attempt.value>=10){
+        setMessage('You Lose!:(');
+        showAnswer(false);
+        showReplay();
+        }
+      else{
+        setMessage('Incorrect!, Try again.');
+      }
+    }
 }
 
 function setHiddenFields(){
-    answer=Math.floor(Math.random()*10000);
-    while(answer.nodeValue.length<4){
-        answer.value+=0+answer.value.tostring();
+    attempt.value=0;
+    answer.value=Math.floor(Math.random()*10000);
+    while(answer.value.length<4){
+        answer.value = "0" + answer.value.toString();
     }
+    console.log('Generated answer: ' + answer.value);
+    
 }
+    
+
+function setMessage(message){
+document.getElementById('message').innerHTML=message;
+}
+
+function showAnswer(success){
+let code=document.getElementById('code');
+if(success){
+    code.className += 'success';
+}else{
+    code.className +='failure';
+}
+code.innerHTML=answer.value;
+
+}
+
+function showReplay(){
+    document.getElementById('guessing-div').style.display="none";
+    document.getElementById('replay-div').style.display="block";
+}
+
+function validateInput(userGuess){
+    if(userGuess.length==4){
+        return true;
+    }
+    else{
+        setMessage("Guesses must be exactly 4 characters long.");
+        return false;
+  }
+  
+}
+function getResults(input){
+    let html='<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+    for(i=0;i<input.length;i++){
+        if(input.charAt(i)==answer.value.charAt(i)){
+            html +='<span class="glyphicon glyphicon-ok"></span>';
+        }else if(answer.value.indexOf(input.charAt(i)>-1)){
+            console.log("transfer icon identify charat"+input.charAt(i));
+            html +='<span class="glyphicon glyphicon-transfer"></span>';
+        }
+        else{
+            html+='<span class="glyphicon glyphicon-remove"></span>';
+        }
+    }
+        html += '</div></div>';
+        document.getElementById('results').innerHTML += html;
+
+        if(input==answer.value){
+            return true;
+        }
+        else{
+        return false;
+        }
+    }
+    
+
+
+
+
+
+
+
+
 
 //implement new functions here
